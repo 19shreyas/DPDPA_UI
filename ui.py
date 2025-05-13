541da6c450cc2d77cdd29876b5a6e95fafc256d3
19shreyas <91390993+19shreyas@users.noreply.github.com>
2025-05-13 15:51:15 +0530

    Update ui.py

--- ui.py
+++ ui.py
@@ -72,9 +72,8 @@ st.markdown("""
         width: 160px;
     }
     </style>
-    <img id="ey-logo-global" src="images/EY-Parthenon_idpWq1a8hl_0.png" />
     """, unsafe_allow_html=True)
-
+st.image("images/EY-Parthenon_idpWq1a8hl_0.png", width=160)
 set_custom_css()
 #st.sidebar.image(".images/EY-Parthenon_idpWq1a8hl_0.png", width=200)
 col_logo, _ = st.sidebar.columns([4, 1])
