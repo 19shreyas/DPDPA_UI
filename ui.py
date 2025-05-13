6242c1e53ce1968aba3c099bccd0767a54654f42
19shreyas <91390993+19shreyas@users.noreply.github.com>
2025-05-13 14:55:20 +0530

    Update ui.py

--- ui.py
+++ ui.py
@@ -61,7 +61,7 @@ def set_custom_css():
 # --- Sidebar Navigation ---
 st.set_page_config(page_title="DPDPA Compliance Tool", layout="wide")
 set_custom_css()
-st.sidebar.image(".images/EY-Parthenon_idpWq1a8hl_0.png", width=140)
+st.sidebar.image(".images/EY-Parthenon_idpWq1a8hl_0.png", width=120)
 st.sidebar.title("📘 Navigation")
 
 menu = st.sidebar.radio("Go to", [
