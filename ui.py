4abad5bfcbd1478da3af98b5b8b3a8b9ba2cd31f
19shreyas <91390993+19shreyas@users.noreply.github.com>
2025-05-13 16:22:48 +0530

    Update ui.py

--- ui.py
+++ ui.py
@@ -40,7 +40,7 @@ menu = st.sidebar.radio("Go to", [
 st.sidebar.markdown("<br><br><br><br><br><br><br><br><br><br><br><br><br>", unsafe_allow_html=True)
 #st.sidebar.image(".images/EY-Parthenon_idpWq1a8hl_0.png", width=250)
 st.sidebar.markdown("""
-    <div style='padding: 16px 12px 0px 12px;'>
+    <div style='padding: 0px 12px 0px 12px;'>
         <img src='https://i.postimg.cc/j2dv9kZ2/EY-Parthenon-idp-Wq1a8hl-0.png' width='250'>
     </div>
 """, unsafe_allow_html=True)
