bc6d9508a0741c18e4c2a71fa68abb696872d98c
19shreyas <91390993+19shreyas@users.noreply.github.com>
2025-05-13 16:02:11 +0530

    Update ui.py

--- ui.py
+++ ui.py
@@ -62,22 +62,10 @@ def set_custom_css():
 
 # --- Sidebar Navigation ---
 st.set_page_config(page_title="DPDPA Compliance Tool", layout="wide")
-st.image(".images/EYP Logo-white-bg.png", width=250)
+st.image(".images/EYP Logo-white-bg.png", width=300)
 set_custom_css()
-#st.sidebar.image(".images/EY-Parthenon_idpWq1a8hl_0.png", width=200)
-col_logo, _ = st.sidebar.columns([4, 1])
-# with col_logo:
-#     st.image(".images/EY-Parthenon_idpWq1a8hl_0.png", width=500)
-# st.sidebar.markdown(
-#     """
-#     <div style="display: flex; justify-content: flex-start; align-items: flex-start; margin-bottom: 0.5rem; padding-top: 0;">
-#         <img src=".images/EY-Parthenon_idpWq1a8hl_0.png" width="140"/>
-#     </div>
-#     """,
-#     unsafe_allow_html=True
-# )
 
-st.sidebar.title("📘 Navigation")
+st.sidebar.title("Navigation")
 
 menu = st.sidebar.radio("Go to", [
     "Homepage",
